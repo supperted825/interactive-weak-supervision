3edf6d5b26405bd4ef3e6a4489a023fcfbc2842f
Jonathan Tan <jonathantyt@gmail.com>
2023-03-07 15:36:03 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+setup(
+    name='iws',
+    packages=find_packages(include=['iws']),
+    version='0.2.1',
+    description='PIP Installable Interactive Weak Supervision',
+    author='Jonathan',
+    license='MIT',
+    install_requires=[         
+        'pandas',         
+        'python-dotenv',
+        'scipy',
+        'numpy',
+        'spacy',
+        'ipywidgets',
+        'snorkel',
+        'sklearn',
+        'ipywidgets',
+        'torch',
+        'notebook',
+        'beautifulsoup4'
+        'matplotlib'
+    ],
+)
